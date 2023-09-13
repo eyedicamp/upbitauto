2136a012beadfbc094881182794fb9c5d2233981
LAPTOP-SSY\slsl9 <slsl980903@gmail.com>
2023-09-13 13:46:11 +0900

    key 내림

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import jwt   # PyJWT 
 import uuid
 
-payload = {
-    'access_key': '''-----BEGIN ACCESS KEY-----
-3EhTwzfqMPbtQQpXryqLjWiSeufYyRXKUAWeUiBS
------END ACCESS KEY-----''',
-    'nonce': str(uuid.uuid4()),
-}
 
-jwt_token = jwt.encode(payload, '''-----BEGIN SECRET KEY-----
-ygxjeIZ6eWNerlNutkDje1xPhbIxK00jxqmyE53Q
------END SECRET KEY-----''')
-authorization_token = 'Bearer {}'.format(jwt_token)
 
 
 
